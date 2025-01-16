8954cba0a1b24215102fa8ec77497beb10b99599
Mrsingh422 <sagarsinghbankhandi@gmail.com>
2025-01-16 11:20:09 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import Tiktaktoe from './components/TikTokToe';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Tiktaktoe />
+     
+    </div>
+  );
+}
+
+export default App;
